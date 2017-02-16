265d369e62ca365ef8ff2710cef2bf2f8363bf94
Raja Vijaya Saradhi Ch <rvrajavijay@gmail.com>
2017-02-16 20:59:26 +0530

    Firebase Setup and Login Form Init

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import CONFIG from './config'; // Importing firebase config from const from another file. 
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
+
+    componentWillMount() {
+        firebase.initializeApp({ CONFIG });
+    }
+
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>Hello World</Text>
+                <LoginForm />
             </View>
         );
     }
